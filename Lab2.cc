9a13a9ee7df91bbad2cf852a8e9df834271f3a3f
HBryATx <a.twardosz98@gmail.com>
2018-10-16 14:21:43 +0200

    poprawiona wersja poprawionej wersji

--- Lab2.cc
+++ Lab2.cc
@@ -1,15 +1,15 @@
-
 #include <iostream>
 using namespace std;
 
 //To jest komentarz do tego pliku. Ten plik jest ewidentnie źle sformatowany i należy to koniecznie poprawić, pamiętając o nazwach zmiennych
 //dopuszczalnej długości linii, znakach z kodu ANSI, wcięciach i dbalości o wysokiej jakosci komentarze
 
-int main() { 
+int main() 
+{ 
     int PierwszaZmienna = 5;
     int DrugaZmienna = 7;
-    int WynikDziałania = PierwszaZmienna + DrugaZmienna;
+    int WynikDzialania = PierwszaZmienna + DrugaZmienna;
 
-    cout << "Wynik Dodawania Dwóch Liczb To: " << WynikDziałania << endl; // Wypisuje wynik dzialania
+    cout << "Wynik Dodawania Dwóch Liczb To: " << WynikDzialania << endl; // Wypisuje wynik dzialania
     return 0;
 }
